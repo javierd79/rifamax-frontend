c093d78b63939de12c139b0b7a3e1407eb8705a8
Javier <javierdiazt406@gmail.com>
2022-09-25 00:15:25 -0400

    changes

--- src/pages/users.js
+++ src/pages/users.js
@@ -3,11 +3,12 @@ import { PopoverBody } from "reactstrap";
 import { Popover, PopoverHeader } from "@chakra-ui/react";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
-import { BsFillPersonFill } from "react-icons/bs";
-// import { FaEdit, BsTrash } from "react-icons/fa";
+import { BsFillPersonFill, BsTrash } from "react-icons/bs";
+import { FaEdit } from "react-icons/fa";
 import { useAuthState, useAuthDispatch, logout } from "../context/auth";
 import "../assets/scss/pages/users.scss";
 import axios from "axios";
+import Modal from "../components/modal";
 
 function Users() {
   const [isOpen, setIsOpen] = useState(false);
@@ -111,27 +112,68 @@ function Users() {
         <div className="container-fluid users">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Usuarios</h5>
-              <hr/>
+              <h5 className="card-title title">Usuarios</h5>
+              <hr />
               <div className="table-responsive">
-                <table className="table table-striped">
+                <table className="table">
                   <thead>
                     <tr>
+                      <th scope="col">ID</th>
                       <th scope="col">Nombre</th>
                       <th scope="col">Correo</th>
                       <th scope="col">Rol</th>
+                      <th scope="col">Status</th>
                       <th scope="col">Acciones</th>
                     </tr>
                   </thead>
                   <tbody>
                     {users.map((user) => (
                       <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.role}</td>
+                        <th scope="row" className="body-item">
+                          {user.id}
+                        </th>
+                        <td className="body-item">{user.name}</td>
+                        <td className="body-item">{user.email}</td>
+                        <td className="body-item">{user.role}</td>
                         <td>
-                          <button className="btn btn-primary">Editar</button>
-                          <button className="btn btn-danger">Eliminar</button>
+                          {user.status ? (
+                            <span className="badge-activo badge">Activo</span>
+                          ) : (
+                            <span className="badge-inactivo badge">Inactivo</span>
+                          )}
+                        </td>
+                        <td className="body-btn">
+                          <Modal
+                            btnColor="primary"
+                            centered={true}
+                            classBtn="me-2"
+                            buttonTitle=<FaEdit />
+                            title="Editar usuario"
+                          >
+                            <h6>
+                              Una vez eliminado este usuario se borraran todos
+                              su atributos relacionados: Rifas, Sorteos,
+                              Agencias, Riferos, Taquilla
+                            </h6>
+                            <button className="btn btn-danger w-100">
+                              Eliminar
+                            </button>
+                          </Modal>
+                          <Modal
+                            btnColor="danger"
+                            centered={true}
+                            buttonTitle=<BsTrash />
+                            title="Estas seguro de eliminar este usuario?"
+                          >
+                            <h6>
+                              Una vez eliminado este usuario se borraran todos
+                              su atributos relacionados: Rifas, Sorteos,
+                              Agencias, Riferos, Taquilla
+                            </h6>
+                            <button className="btn btn-danger w-100">
+                              Eliminar
+                            </button>
+                          </Modal>
                         </td>
                       </tr>
                     ))}
